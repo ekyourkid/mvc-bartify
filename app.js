aad9fadbaad475a63a44d7957fb638b947bcfcd7
albarmo <moerhamsa@gmail.com>
2022-12-04 17:14:37 +0700

    feat:add notes

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ let command = process.argv[2]
 let input = process.argv[3]
 let payload = process.argv[4]
 
+// list song, add song , update song, delete song , play song
+// song has property : id, title, band, genre
+
 switch (command) {
   case 'list':
     console.log('list of songs....')
